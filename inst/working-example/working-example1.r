824b8f2c96f5379413bfb5fe01af637ab3d60747
Michael Kane <kaneplusplus@gmail.com>
2018-12-15 16:39:21 -0500

    Yeah

--- inst/working-example/working-example1.r
+++ inst/working-example/working-example1.r
@@ -16,19 +16,19 @@ eb
 # Plot the posteriors
 plot_density(eb)
 
-plot_exchangeability(eb, size = 1.5, label = TRUE)
+plot_exchangeability(eb, size = 3, label = TRUE)
 
 # Fit the MEM model using full Bayes.
 fb <- mem_full_bayes(responses = vemu_wide$responders, 
                      size = vemu_wide$evaluable,
-                     name = vemu_wide$baskets
+                     name = vemu_wide$baskets,
                      p0 = 0.15)
 
 
 # Plot the posteriors
 plot_density(fb)
 
-plot_posterior_exchangeability(fb, size = 1.5, label = TRUE)
+plot_posterior_exchangeability(fb, size = 3, label = TRUE)
 
 plot_exchangeability(fb, size = 1.5, label = TRUE)
 
