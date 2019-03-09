3238c111afc29c724a54d780b6efee162c9553a8
MayLaptop <nchen160@gmail.com>
2019-03-09 12:13:57 -0600

    Added the MCMC test removed unused codes.

--- tests/testthat/test-mh.r
+++ tests/testthat/test-mh.r
@@ -8,10 +8,6 @@ context("Metropolis-Hasting test")
 # Load the reference outputs.
 load("../../inst/test-data/fitMCMC1_reference.rda")
 load("../../inst/test-data/fitMCMC2_reference.rda")
-
-
-## Vectors of Observed number of Responses (X) and Patients (N)
-#Data <- dget("../../inst/code-from-brian/MHAlgorithm/Vemu-data.txt")
 data(vemu_wide)
 
 mem_fit1 <- mem_full_bayes_mcmc(
@@ -19,26 +15,26 @@ mem_fit1 <- mem_full_bayes_mcmc(
   name = c("NSCLC ", "CRC.v ", "CRC.vc", "  BD  ", "ED.LH ", " ATC  "),
   p0 = 0.15, Initial = NA
 )
-#
-#
-#expect_equal(MHResult1$PEP, fit.MCMC.1$PEP)
-#expect_equal(MHResult1$HPD, fit.MCMC.1$HPD, tolerance = 5e-2)
-#expect_equal(MHResult1$CDF, fit.MCMC.1$CDF)
-#expect_equal(MHResult1$ESS, fit.MCMC.1$ESS, tolerance = 5e-2)
-#expect_equal(MHResult1$ESS2, fit.MCMC.1$ESS2, tolerance = 5e-2)
+
+
+expect_equal(mem_fit1$basketwise$PEP, fit.MCMC.1$PEP)
+expect_equal(mem_fit1$basketwise$HPD, fit.MCMC.1$HPD, tolerance = 5e-2)
+expect_equal(mem_fit1$basketwise$CDF, fit.MCMC.1$CDF)
+expect_equal(mem_fit1$basketwise$ESS, fit.MCMC.1$ESS, tolerance = 5e-2)
 
 
 
-#init <- dget("../../inst/code-from-brian/MHAlgorithm/MCMCtestInitial.txt")
+init <- dget("../../inst/test-data/MCMCtestInitial.txt")
 
-#MHResult2 <- mem_full_bayes_mcmc(
-#  responses = Data$X, size = Data$N, name = c("NSCLC ", "CRC.v ", "CRC.vc", "  BD  ", "ED.LH ", " ATC  "),
-#  p0 = 0.15, Initial = init
-#)
+mem_fit2 <- mem_full_bayes_mcmc(
+  responses = vemu_wide$responders, size = vemu_wide$evaluable,
+  name = c("NSCLC ", "CRC.v ", "CRC.vc", "  BD  ", "ED.LH ", " ATC  "),
+  p0 = 0.15, Initial = init
+)
 
 
-#expect_equal(MHResult2$PEP, fit.MCMC.2$PEP)
-#expect_equal(MHResult2$HPD, fit.MCMC.2$HPD, tolerance = 5e-2)
-#expect_equal(MHResult2$CDF, fit.MCMC.2$CDF)
-#expect_equal(MHResult2$ESS, fit.MCMC.2$ESS, tolerance = 5e-2)
-#expect_equal(MHResult2$ESS2, fit.MCMC.2$ESS2, tolerance = 5e-2)
+expect_equal(mem_fit2$basketwise$PEP, fit.MCMC.2$PEP)
+expect_equal(mem_fit2$basketwise$HPD, fit.MCMC.2$HPD, tolerance = 5e-2)
+expect_equal(mem_fit2$basketwise$CDF, fit.MCMC.2$CDF)
+expect_equal(mem_fit2$basketwise$ESS, fit.MCMC.2$ESS, tolerance = 5e-2)
+
