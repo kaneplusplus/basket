132923fc32ca3e524c78bbab415ac6dfdf2e8ce8
MayLaptop <nchen160@gmail.com>
2019-03-25 22:29:26 -0500

    Some fix on long testing time and exact seed.

--- tests/testthat/test-exact.r
+++ tests/testthat/test-exact.r
@@ -15,7 +15,7 @@ exact_res <- mem_exact(
   responses = vemu_wide1$responders,
   size = vemu_wide1$evaluable,
   name = c("NSCLC", "CRC (vemu)", "CRC (vemu+cetu)"),
-  p0 = 0.25, seed=1000
+  p0 = 0.25
 )
 
 fb <- exact_res$basket
@@ -24,6 +24,8 @@ expect_equal(fb$PEP, fb_reference$basketwise$PEP)
 expect_equal(fb$HPD, fb_reference$basketwise$HPD, tolerance = 5e-2)
 expect_equal(fb$post.prob, fb_reference$basketwise$post.prob)
 expect_equal(fb$ESS, fb_reference$basketwise$ESS)
+summary(exact_res)
+print(exact_res)
 
 res1 <- update(exact_res, p0=0.15)
 
