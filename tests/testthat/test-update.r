61bfec6b10e3a28dd87d26bbc2a1c735f826406a
Michael Kane <kaneplusplus@gmail.com>
2019-04-05 16:32:37 -0400

    Initial commit.

---
+++ tests/testthat/test-update.r
@@ -0,0 +1,16 @@
+library(testthat)
+library(basket)
+
+context("Update models")
+
+data(vemu_wide)
+
+
+mh1 <- mem_mcmc(responses = vemu_wide$responders, 
+                size = vemu_wide$evaluable,
+                name= vemu_wide$baskets,
+                p0 = c(0.15, 0.15, 0.15, 0.2, 0.15, 0.15), 
+                Initial = NA)
+
+mh2 <- update_p0(mh1, 0.25)
+
